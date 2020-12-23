1312e906cd9d76ce10346b48552c641de49acc28
Nikita-neuron <64783326+Nikita-neuron@users.noreply.github.com>
2020-12-23 09:52:05 +0300

    add some comments

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,70 @@
 export const radioPlayerInit = () => {
-  
+  // получаем элементы со страницы
+  const radio = document.querySelector('.radio');
+  const radioCoverImg = document.querySelector('.radio-cover__img');
+  const radioHeaderBig = document.querySelector('.radio-header__big');
+  const radioNavigation = document.querySelector('.radio-navigation');
+  const radioItem = document.querySelectorAll('.radio-item');
+  const radioStop = document.querySelector('.radio-stop');
+
+  // создаём объект Audio
+  const audio = new Audio();
+  audio.type = 'audio/aac';
+
+  // деактивируем кнопку стопа
+  radioStop.disabled = true;
+
+  const changeIconPlay = () => {
+    // смена иконки сиопа
+    // и анимации динамика
+    if  (audio.paused) {
+      radio.classList.remove('play');
+      radioStop.classList.add('fa-play');
+      radioStop.classList.remove('fa-stop');
+    } else {
+      radio.classList.add('play');
+      radioStop.classList.remove('fa-play');
+      radioStop.classList.add('fa-stop');
+    }
+  }
+
+  const selectItem = elem => {
+    // стилизация иконок
+    radioItem.forEach(item => item.classList.remove('select'));
+    elem.classList.add('select');
+  }
+
+  radioNavigation.addEventListener('change', event => {
+    // получение родителя выбранного элемента
+    const target = event.target;
+    const parent = target.closest('.radio-item');
+    selectItem(parent);
+
+    // смена заголовка радио
+    const title = parent.querySelector('.radio-name').textContent;
+    radioHeaderBig.textContent = title;
+
+    // смена картинки радио
+    const urlImg = parent.querySelector('.radio-img').src;
+    radioCoverImg.src = urlImg;
+
+    radioStop.disabled = false;
+
+    // меняем радиостанцию
+    audio.src = target.dataset.radioStantion;
+
+    // начинаем воспроизведение
+    audio.play();
+    changeIconPlay();
+  });
+
+  radioStop.addEventListener('click', () => {
+    // отслеживание событий нажатия на кнопку стоп
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+    changeIconPlay();
+  });
 }
--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,4 +1,5 @@
 export const videoPlayerInit = () => {
+  // получение элементов со страницы
   const videoPlayer = document.querySelector('.video-player');
   const videoButtonPlay = document.querySelector('.video-button__play');
   const videoButtonStop = document.querySelector('.video-button__stop');
@@ -7,6 +8,7 @@ export const videoPlayerInit = () => {
   const videoTimeTotal = document.querySelector('.video-time__total');
 
   const toggleIcon = () => {
+    // смена иконки стоп при проигрывании
     if (videoPlayer.paused) {
       videoButtonPlay.classList.remove('fa-pause');
       videoButtonPlay.classList.add('fa-play');
@@ -17,6 +19,7 @@ export const videoPlayerInit = () => {
   }
 
   const togglePlay = () => {
+    // остановка или воспроизведение видео
     if (videoPlayer.paused) {
       videoPlayer.play();
     } else {
@@ -25,40 +28,50 @@ export const videoPlayerInit = () => {
   }
 
   const stopPlay = () => {
+    // остановка плеера
     videoPlayer.pause();
     videoPlayer.currentTime = 0;
   }
 
+  // добавление нулей в запись времени
   const addZero = n => n < 10 ? '0' + n : n;
 
+  // отслеживание событий при нажатии на видео или на кнопку
   videoPlayer.addEventListener('click', togglePlay);
   videoButtonPlay.addEventListener('click', togglePlay);
 
+  // смена иконки при проигрывании плеера
   videoPlayer.addEventListener('play', toggleIcon);
   videoPlayer.addEventListener('pause', toggleIcon);
 
   videoButtonStop.addEventListener('click', stopPlay);
 
   videoPlayer.addEventListener('timeupdate', () => {
+    // получение текущего времени и общего времени
     const currentTime = videoPlayer.currentTime;
     const duration = videoPlayer.duration;
 
+    // установка ползунка по времени
     videoProgress.value = (currentTime / duration) * 100;
 
+    // получение минут и секунд видео
     let minutePassed = Math.floor(currentTime / 60);
     let secondPassed = Math.floor(currentTime % 60);
 
     let minuteTotal = Math.floor(duration / 60);
     let secondTotal = Math.floor(duration % 60);
 
+    // установка текущего и общего времени
     videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondPassed)}`;
     videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondTotal)}`;
   });
 
   videoProgress.addEventListener('input', () => {
+    // отслеживание перемещение пользователем ползунка
     const duration = videoPlayer.duration;
     const value = videoProgress.value;
 
+    // перемотка в зависимости от установленного значения
     videoPlayer.currentTime = (value * duration) / 100;
   });
 }
